99219e120f39f20e4d3bc8053b8b2dd357d81780
Ari Ade Supriyatna <ariadesupriyatna698@gmail.com>
2025-05-02 16:20:46 +0700

    Create Bot.Push.View.Github.sh

---
+++ Bot.Push.View.Github.sh
@@ -0,0 +1,11 @@
+:
+https://s.id/ariadesupriyatna
+https://nolduakali.blogspot.com
+kenalin aku Ade dan aku suka riset kelemahan di bidang it
+:
+
+while true; do
+  clear
+  curl -X GET https://camo.githubusercontent.com/982b38687aa7246e714fa1603ee726e1d2e2ce4f902f013861a2abe7b3883eb0/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d6e6f6c6475616b616c69 -o /dev/null
+  sleep 3
+done
