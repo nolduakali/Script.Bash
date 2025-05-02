3de9325c173115594d195f42fc7a69ab48d894d1
Ari Ade Supriyatna <ariadesupriyatna698@gmail.com>
2025-05-02 19:44:37 +0700

    Update Bot.Push.View.Github.sh

--- Bot.Push.View.Github.sh
+++ Bot.Push.View.Github.sh
@@ -9,3 +9,11 @@ while true; do
   curl -X GET https://camo.githubusercontent.com/982b38687aa7246e714fa1603ee726e1d2e2ce4f902f013861a2abe7b3883eb0/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d6e6f6c6475616b616c69 -o /dev/null
   sleep 3
 done
+
+#penggunaan
+#tambahkan dibawah ini di file readme.md, lihat halaman depan github punya ku
+#![](https://komarev.com/ghpvc/?username=nolduakali)
+#klik dan tahan pada yang barusan kita buat diatas, dan kemudian buka di tab baru 
+#salin url nya 
+#ganti url script ini dengan url milikmu 
+#jalankan, bisa ubah sleep 3 jadi 5 atau 10 atau 1 terserah
